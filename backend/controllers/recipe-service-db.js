597de7f288e89912f00bd0c8f8b0a1af49028d85
Mei LinLi <meilynly00@gmail.com>
2021-11-28 21:52:23 -0800

    crud recipe

--- backend/controllers/recipe-service-db.js
+++ backend/controllers/recipe-service-db.js
@@ -1,10 +1,71 @@
 const recipe_dao = require('../models/dao/recipe-dao');
+const {recipe_model} = require("../models/all_models");
 
 module.exports = (app) => {
 
+    // create
+    const createNewRecipe = (req, res) => {
+        const newRecipe = req.body;
+        recipe_dao.createNewRecipe(newRecipe);
+        res.sendStatus(200);
+    }
+
+    // read
     const findAllRecipes = (req, res) => {
         recipe_dao.findAllRecipes()
-            .then(authors => res.json(authors));
+            .then(recipes => res.json(recipes));
     }
+
+    const findRecipeById = (req, res) => {
+        const recipeId = req.params.id;
+        recipe_dao.findRecipeById(recipeId)
+            .then(recipes => res.json(recipes));
+    }
+
+
+    const findRecipeByRecipeName = (req, res) => {
+        const recipeName = req.params.recipeName;
+        recipe_dao.findRecipeByName(recipeName)
+            .then(recipe => res.status(200).json(recipe));
+    }
+
+
+    const searchRecipesByName = (req, res) => {
+        const recipeName = req.params.recipeName;
+        recipe_dao.searchRecipesByName(recipeName)
+            .then(recipes => res.status(200).json(recipes));
+    }
+
+    // update
+    const updateRecipeInfo = (req, res) => {
+        const recipeId = req.params.id;
+        const recipeInfo = req.body;
+        recipe_dao.updateRecipeInfo(recipeId, recipeInfo)
+        res.sendStatus(200);
+    }
+
+    // delete
+    const deleteRecipe = (req, res) => {
+        const recipeId = req.params.id;
+        recipe_dao.deleteRecipe(recipeId);
+        res.sendStatus(200);
+    }
+
+
+    /////////// APIS ///////////
+
+    //create
+    app.post("/rest/recipes", createNewRecipe);
+
+    // read
     app.get('/rest/recipes', findAllRecipes);
+    app.get('/rest/recipes/:id', findRecipeById);
+    app.get('/rest/recipes/findRecipe/:recipeName', findRecipeByRecipeName);
+    app.get('/rest/recipes/searchRecipe/:recipeName', searchRecipesByName);
+
+    // update
+    app.put("/rest/recipes/:id", updateRecipeInfo);
+
+    // delete
+    app.delete("/rest/recipes/:id", deleteRecipe);
 };
--- backend/models/dao/recipe-dao.js
+++ backend/models/dao/recipe-dao.js
@@ -1,9 +1,35 @@
 //const recipe_model = require('../recipe-model');
 const {recipe_model} = require("../all_models.js");
 
+// CREATE
+module.exports.createNewRecipe = (recipe) => {
+    recipe_model.create(recipe)
+        .catch(err => console.log(err));
+}
 
-const findAllRecipes = () => recipe_model.find();
+module.exports.findAllRecipes = () => recipe_model.find();
 
-module.exports = {
-    findAllRecipes
-};
+module.exports.findRecipeById = (recipeId) => {
+    return recipe_model.findById(recipeId);
+}
+
+module.exports.findRecipeByName = (recipeName) => {
+    return recipe_model.findOne({Name: recipeName});
+}
+
+module.exports.searchRecipesByName = (name) => {
+    return recipe_model.find({Name : {$regex : name}});
+}
+
+// UPDATE
+module.exports.updateRecipeInfo = (recipeID, recipe) => {
+    console.log(`recipe id is: ${recipeID}, updated recipe info: ${recipe}`);
+    recipe_model.updateOne({_id: recipeID}, {$set: recipe})
+        .catch(err => console.log(err));
+}
+
+
+// DELETE
+module.exports.deleteRecipe = (id) => {
+    recipe_model.findByIdAndDelete({_id: id});
+}
