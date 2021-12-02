dc7081be4d2126be1b290147ef0e57f3f90ee651
Justin Lee <jclee1996@gmail.com>
2021-12-01 17:06:00 -0800

    adding all modified files

--- frontend/src/components/RecipeList/RecipeDetail.js
+++ frontend/src/components/RecipeList/RecipeDetail.js
@@ -3,29 +3,30 @@ import "./index.css";
 
 const RecipeDetail = ({recipe}) => {
   <div className="container">
-    <p>
-      {recipe.Name}
-    </p>
-    <p>
-      {recipe.AuthorId}
-    </p>
-    {
-      recipe.Keywords.map(keyword =>
-        <p>{keyword}</p>
-      )
-    }
-    <p>{recipe.DatePublished}</p>
-    <p>{recipe.Description}</p>
-    <p>{recipe.Images}</p>
-    <p>{recipe.RecipeCategory}</p>
-    <p>{recipe.RecipeIngredientQuantities}</p>
-    <p>{recipe.RecipeIngredientParts}</p>
-    <p>{recipe.AggregatedRating}</p>
-    <p>{recipe.ReviewCount}</p>
-    <p>{recipe.Calories}</p>
-    <p>{recipe.RecipeInstructions}</p>
-    <p>{recipe.LikeNum}</p>
-    <p>{recipe.StarNum}</p>
+    {/*<p>*/}
+    {/*  {recipe.Name}*/}
+    {/*</p>*/}
+    {/*<p>*/}
+    {/*  {recipe.AuthorId}*/}
+    {/*</p>*/}
+    {/*{*/}
+    {/*  recipe.Keywords.map(keyword =>*/}
+    {/*    <p>{keyword}</p>*/}
+    {/*  )*/}
+    {/*}*/}
+    {/*<p>{recipe.DatePublished}</p>*/}
+    {/*<p>{recipe.Description}</p>*/}
+    {/*<p>{recipe.Images}</p>*/}
+    {/*<p>{recipe.RecipeCategory}</p>*/}
+    {/*<p>{recipe.RecipeIngredientQuantities}</p>*/}
+    {/*<p>{recipe.RecipeIngredientParts}</p>*/}
+    {/*<p>{recipe.AggregatedRating}</p>*/}
+    {/*<p>{recipe.ReviewCount}</p>*/}
+    {/*<p>{recipe.Calories}</p>*/}
+    {/*<p>{recipe.RecipeInstructions}</p>*/}
+    {/*<p>{recipe.LikeNum}</p>*/}
+    {/*<p>{recipe.StarNum}</p>*/}
+
   </div>
 }
 
