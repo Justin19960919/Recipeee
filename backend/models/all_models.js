dfd056df3b501b76384d453f7fed41ecfcb8fb28
Mei LinLi <meilynly00@gmail.com>
2021-12-02 22:43:56 -0800

    add review crud

--- backend/models/all_models.js
+++ backend/models/all_models.js
@@ -5,19 +5,22 @@ const mongoose = require('mongoose');
 const recipe_schema = require('../schemas/recipe-schema');
 const author_schema = require('../schemas/author-schema');
 const user_schema = require('../schemas/user-schema');
+const review_schema = require('../schemas/user-schema');
 
 
 // define models
 const recipe_model = mongoose.model('RecipeModel', recipe_schema);
 const author_model = mongoose.model('AuthorModel', author_schema);
 const user_model = mongoose.model('UserModel', user_schema);
+const review_model = mongoose.model('ReviewModel', review_schema);
 
 
 
 module.exports = {
   recipe_model,
   author_model,
-  user_model
+  user_model,
+  review_model
 }
 
 
