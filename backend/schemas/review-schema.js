582456d0c79518d3874926877777b0deb76375d5
meilynly <37729900+meilynly@users.noreply.github.com>
2021-12-08 09:46:28 -0800

    AuthorId
    keep AuthorId in review schema, in case it is already been used

--- backend/schemas/review-schema.js
+++ backend/schemas/review-schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const review_schema = new mongoose.Schema({
     // _id: Number,
     RecipeId: Number,
-    // AuthorId: Number,
+    AuthorId: Number,
     UserId: String,
     Rating: Number,
     Review: String,
