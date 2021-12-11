6636ef02618190ee3643a990bbe5170f2bbe0b51
JasonKTChen <49644422+JasonKTChen@users.noreply.github.com>
2021-12-10 17:37:41 -0800

    added shit

--- frontend/src/components/ReachOutForm/index.js
+++ frontend/src/components/ReachOutForm/index.js
@@ -4,44 +4,66 @@ const ReachOutForm = () => {
   return (
     <div>
       <div className="form-page">
-      <div className="about-us">
-        <h4 className="about-us-title">About Recipeee</h4>
-        <p className="about-us-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock</p>
+        <div className="about-us">
+          <h4 className="about-us-title">About Recipeee</h4>
+          <p className="about-us-content">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock
+          </p>
+        </div>
+
+        <div className="form">
+          <p className="contact-form-header">Contact Form</p>
+          <form
+            action="https://formspree.io/f/xleaqkbq"
+            method="POST"
+            className="reveal-content contact-form"
+          >
+            <div>
+              {/*email */}
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="_replyto"
+                  id="email"
+                  placeholder="Email"
+                />
+              </div>
+
+              {/*subject*/}
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="subject"
+                  placeholder="Subject"
+                />
+              </div>
+
+              {/*message*/}
+              <div className="form-group">
+                <textarea
+                  className="form-control"
+                  rows="5"
+                  name="message"
+                  placeholder="Enter your message"
+                ></textarea>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg submit-button"
+              >
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="form">
-      <p className="contact-form-header">Contact Form</p>
-      <form action="https://formspree.io/f/xleaqkbq" method="POST"
-          className="reveal-content contact-form">
-        <div>
-          {/*email */}
-          <div className="form-group">
-            <input type="email" className="form-control" name="_replyto" id="email"
-                   placeholder="Email"/>
-          </div>
-
-          {/*subject*/}
-          <div className="form-group">
-            <input type="text" className="form-control" id="subject"
-                   placeholder="Subject"/>
-          </div>
-
-          {/*message*/}
-          <div className="form-group">
-            <textarea className="form-control" rows="5" name="message"
-                      placeholder="Enter your message"></textarea>
-          </div>
-
-          <button type="submit"
-                  className="btn btn-primary btn-lg submit-button">Send
-          </button>
-          </div>
-      </form>
     </div>
-  </div>
-  </div>
-
   );
-}
-
+};
 
 export default ReachOutForm;
--- frontend/src/components/Review/index.js
+++ frontend/src/components/Review/index.js
@@ -1,45 +1,51 @@
 import "./review.css";
 
 const Review = () => {
+  const reviews = [
+    {
+      Image: "url",
+      position: "left",
+    },
+  ];
 
-    const reviews = [
-        {
-            Image: "url",
-            position: "left",
-        }
-    ]
-
-
-
-    return (
-        <div className="review">
-            <div className="container first">
-                <img className="review-image" src="/pic/lasagnia.jpeg" alt="food" />
-                <div className="comment-container">
-                    <h4 className="review-title">Mexican Lasagna</h4>
-                    <p className="comment text-truncate">"Here's what one recipe reviewer had to say: "My husband and I love, love, love this recipe! We make it for just us to eat throughout the week! Thank you!"</p>
-                    <h6 className="author-name">Giovanni</h6>
-                </div>
-
-            </div>
-            <div className="container second">
-                <div className="comment-container">
-                    <h4 className="review-title">Lemon Yogurt Cake</h4>
-                    <p className="comment text-truncate">"This cake was fabulous — very moist with wonderful and not overwhelming lemon flavor. I also love how easy and quick it is to make, plus nearly everyone will have these ingredients on hand."</p>
-                    <h6 className="author-name">Stefano</h6>
-                </div>
-                <img className="review-image" src="/pic/poundcake.jpeg" alt="food" />
-            </div>
-            <div className="container third">
-                <img className="review-image" src="/pic/chicken.jpeg" alt="food" />
-                <div className="comment-container">
-                    <h4 className="review-title">Roman-Style Chicken</h4>
-                    <p className="comment text-truncate">"This is really great! I keep trying to find new recipes for chicken — this one is definitely five-star."</p>
-                    <h6 className="author-name">----- Alessandro</h6>
-                </div>
-            </div>
+  return (
+    <div className="review">
+      <div className="container first">
+        <img className="review-image" src="/pic/lasagnia.jpeg" alt="food" />
+        <div className="comment-container">
+          <h4 className="review-title">Mexican Lasagna</h4>
+          <p className="comment text-truncate">
+            "Here's what one recipe reviewer had to say: "My husband and I love,
+            love, love this recipe! We make it for just us to eat throughout the
+            week! Thank you!"
+          </p>
+          <h6 className="author-name">Giovanni</h6>
         </div>
-
-    )
-}
+      </div>
+      <div className="container second">
+        <img className="review-image" src="/pic/poundcake.jpeg" alt="food" />
+        <div className="comment-container">
+          <h4 className="review-title">Lemon Yogurt Cake</h4>
+          <p className="comment text-truncate">
+            "This cake was fabulous — very moist with wonderful and not
+            overwhelming lemon flavor. I also love how easy and quick it is to
+            make, plus nearly everyone will have these ingredients on hand."
+          </p>
+          <h6 className="author-name">Stefano</h6>
+        </div>
+      </div>
+      <div className="container third">
+        <img className="review-image" src="/pic/chicken.jpeg" alt="food" />
+        <div className="comment-container">
+          <h4 className="review-title">Roman-Style Chicken</h4>
+          <p className="comment text-truncate">
+            "This is really great! I keep trying to find new recipes for chicken
+            — this one is definitely five-star."
+          </p>
+          <h6 className="author-name">----- Alessandro</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Review;
