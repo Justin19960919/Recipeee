7b0eef773593862666339270e6653ffc03d01712
Mei LinLi <meilynly00@gmail.com>
2021-12-11 21:33:03 -0800

    follow schema

--- backend/schemas/follow-schema.js
+++ backend/schemas/follow-schema.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const follow_schema = new mongoose.Schema({
     Username: String,
+    OtherId: mongoose.Schema.Types.ObjectId,
     FollowName: String
 }, {collection: "follows"});
 
